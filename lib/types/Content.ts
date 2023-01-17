1750ec0a35f04a1810fe3f6cafcf62adb39e6548
Bill OConnor <williamboconnor@gmail.com>
2023-01-17 16:02:33 -0600

    Content data types

--- lib/types/Content.ts
+++ lib/types/Content.ts
@@ -1,3 +1,32 @@
 export interface Content<T> {
   content: T;
 }
+
+export type Blog = {
+  title: string;
+  id: string;
+  description: string;
+  image: string;
+  href: string;
+  tags: string[];
+};
+
+export type Course = {
+  title: string;
+  id: string;
+  description: string;
+  image: string;
+  href: string;
+  tags: string[];
+};
+
+export type Project = {
+  title: string;
+  id: string;
+  description: string;
+  image: string;
+  href: string;
+  tags: string[];
+};
+
+export type Resource = Blog | Course | Project;
