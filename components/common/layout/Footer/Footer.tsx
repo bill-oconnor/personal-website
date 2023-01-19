11ad4f7b8d7ea1c820099011f75825df3ddfb47e
Bill OConnor <williamboconnor@gmail.com>
2023-01-18 20:08:53 -0600

    Social links in footer

--- components/common/layout/Footer/Footer.tsx
+++ components/common/layout/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@mantine/core";
+import { Divider, Group, Text } from "@mantine/core";
 import { Section } from "../Section";
 import { useStyles } from "./styles";
 
@@ -7,7 +7,32 @@ export const Footer = () => {
 
   return (
     <Section className={classes.footer}>
-      <Text align="center">Something about copyrights and such</Text>
+      <Group>
+        <a
+          target="_blank"
+          href="https://twitter.com/slick_willyO"
+          rel="noreferrer"
+        >
+          Twitter
+        </a>
+        <Divider orientation="vertical" />
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/william-o-connor512/"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>
+        <Divider orientation="vertical" />
+        <a
+          target="_blank"
+          href="https://github.com/willoconnor18"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </Group>
+      <Text align="center">&copy; 2023 William O&apos;Connor</Text>
     </Section>
   );
 };
