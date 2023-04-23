6f3471c5073d7a372a0b02610c7e61bb7d9300fe
Bill OConnor <williamboconnor@gmail.com>
2023-04-23 18:17:15 -0500

    Use base_url env

--- lib/api/constants.ts
+++ lib/api/constants.ts
@@ -1,4 +1,4 @@
-export const baseURL = "";
+export const baseURL = process.env.BASE_URL;
 export const contentURL = `${baseURL}/api/content`;
 export const contactURL = `${baseURL}/api/contact`;
 export const mediumURL = `${baseURL}/api/medium-articles`;
