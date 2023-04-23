b271e16a926c9d28079902a430d05c36f9a6d485
Bill OConnor <williamboconnor@gmail.com>
2023-04-23 17:58:20 -0500

    remove base url

--- lib/api/constants.ts
+++ lib/api/constants.ts
@@ -1,4 +1,4 @@
-export const baseURL = "http://localhost:3000";
+export const baseURL = "";
 export const contentURL = `${baseURL}/api/content`;
 export const contactURL = `${baseURL}/api/contact`;
 export const mediumURL = `${baseURL}/api/medium-articles`;
