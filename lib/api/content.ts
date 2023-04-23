bc28ac67fff72f6349f83e64cf4175cc817d5299
Bill OConnor <williamboconnor@gmail.com>
2023-04-23 18:10:29 -0500

    Remove async

--- lib/api/content.ts
+++ lib/api/content.ts
@@ -1,8 +1,5 @@
 import { mediumURL } from "./constants";
 
-export async function getContentFromCMS() {
-  const content = await fetch(mediumURL).then((r) => r.json());
-  console.log({ content });
-  return content;
-  // return {};
+export function getContentFromCMS() {
+  return fetch(mediumURL).then((r) => r.json());
 }
