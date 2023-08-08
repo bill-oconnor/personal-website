2e157033e1832a02303a726a3bbbcc39ee181755
Bill OConnor <williamboconnor@gmail.com>
2023-08-08 10:49:37 -0500

    Remove newsletter dashboard

--- content/projects.js
+++ content/projects.js
@@ -10,26 +10,6 @@ export const projects = [
     type: "project",
   },
   {
-    title: "Bitcoin 4 Normies",
-    id: "BFN1",
-    description:
-      "Bitcoin 4 Normies is a guide to Bitcoin for people who want to understand, but aren't ready to read a whole book and are tired of crypto bros. This is an underserved group that needs targeted Bitcoin education materials.",
-    image: "/bitcoin-for-normies.png",
-    href: "https://bitcoin4normies.com",
-    tags: ["Work in Progress"],
-    type: "project",
-  },
-  {
-    title: "Newsletter Dashboard",
-    id: "GND1",
-    description:
-      "Newsletter Dashboard is a simple interface for browsing email newsletters, separate from the rest of your unrelated emails. Reading newsletters and checking emails are two different activities, and are more effectively treated by having separate interfaces.",
-    image: "/newsletter-dashboard.png",
-    href: "https://newsletterdashboard.com",
-    tags: ["Work in Progress"],
-    type: "project",
-  },
-  {
     title: "Hodl Hat",
     id: "HH1",
     description:
@@ -37,6 +17,16 @@ export const projects = [
     image: "/hodl-hat.png",
     href: "https://hodlhat.com",
     tags: [],
+    type: "project",
+  },
+  {
+    title: "Bitcoin 4 Normies",
+    id: "BFN1",
+    description:
+      "Bitcoin 4 Normies is a guide to Bitcoin for people who want to understand, but aren't ready to read a whole book and are tired of crypto bros. This is an underserved group that needs targeted Bitcoin education materials.",
+    image: "/bitcoin-for-normies.png",
+    href: "https://bitcoin4normies.com",
+    tags: ["Work in Progress"],
     type: "project",
   },
   {
